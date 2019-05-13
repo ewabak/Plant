eb5e0d57b192dcac71eaaed43bc72135e45c856b
ewabak <32634853+ewabak@users.noreply.github.com>
2019-05-14 00:24:47 +0200

    api 2

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -13,28 +13,26 @@ export class CatalogComponent implements OnInit {
 
   ngOnInit() {
   }
-  fetchDrinks() {
+  fetchPlants() {
     var scope = this;
 
     var HTTPRequest = new XMLHttpRequest();
-    HTTPRequest.open('GET', 'https://cors.io/?https://trefle.io/api/plants/103505?token=Ymgra1d5M1dCaUlmMWgyME9qNVhTdz09&fbclid=IwAR3FY03yEVzS77Ca1Q9TIbMdMlJhXtpOjhcqcD-MJHAYJXCNcdA3UrJ2p9Q');
+    HTTPRequest.open('GET', 'https://trefle.io/api/plants/103505?token=Ymgra1d5M1dCaUlmMWgyME9qNVhTdz09&fbclid=IwAR3FY03yEVzS77Ca1Q9TIbMdMlJhXtpOjhcqcD-MJHAYJXCNcdA3UrJ2p9Q');
     HTTPRequest.onload = function() {
       var data = JSON.parse(HTTPRequest.responseText);
 
-      var outputDrinks = document.getElementById('drinks_list');
+      var outputPlants = document.getElementById('plants_list');
 
-    outputDrinks.innerHTML = '';
+    outputPlants.innerHTML = '';
 
-        var strCategory = '';
         var outputHTML = '<Label text="' + data.varieties[0].common_name + '" class="m-b-10" ></Label>';
         
-        outputDrinks.innerHTML += outputHTML;
+        outputPlants.innerHTML += outputHTML;
       
 
     };
     HTTPRequest.send();
   }
-
 
   
     
