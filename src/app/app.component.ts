565fb06511bf4e89390bb9c7812307d4c757336f
ewabak <32634853+ewabak@users.noreply.github.com>
2019-05-13 13:43:54 +0200

    poprawki app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,5 @@
 import { Component } from "@angular/core";
-import { Observable, NgModule } from "tns-core-modules/data/observable";
-import * as frame from "tns-core-modules/ui/frame";
-import { RadSideDrawer } from "nativescript-ui-sidedrawer";
-import { Page } from "tns-core-modules/ui/page";
-import {BrowserModule} from '@angular/platform-browser';
-import {HttpModule} from '@angular/http';
-import { AppComponent } from './app.component';
-import { registerElement } from 'nativescript-angular/element-registry';
-import { CardView } from 'nativescript-cardview';
-registerElement('CardView', () => CardView);
+
 
 @Component({
     moduleId: module.id,
@@ -16,27 +7,4 @@ registerElement('CardView', () => CardView);
     templateUrl: "app.component.html"
 })
 export class AppComponent { }
-
-export class GettingStartedViewModel extends Observable {
-    constructor() {
-        super();
-        this.set("mainContentText", "SideDrawer for NativeScript can be easily setup in the XML definition of your page by defining main- and drawer-content. The component"
-            + " has a default transition and position and also exposes notifications related to changes in its state. Swipe from left to open side drawer.");
-    }
-
-    public onOpenDrawerTap() {
-        let sideDrawer: RadSideDrawer = <RadSideDrawer>(frame.topmost().getViewById("sideDrawer"));
-        sideDrawer.showDrawer();
-    }
-
-    public onCloseDrawerTap() {
-        let sideDrawer: RadSideDrawer = <RadSideDrawer>(frame.topmost().getViewById("sideDrawer"));
-        sideDrawer.closeDrawer();
-    }
-}
-
-export function pageLoaded(args) {
-    let page = args.object as Page;
-    page.bindingContext = new GettingStartedViewModel();
-}
 
--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -13,5 +13,33 @@ export class CatalogComponent implements OnInit {
 
   ngOnInit() {
   }
+/*
+  fetchPlants(input) {
+    var scope = this;
 
+    var HTTPRequest = new XMLHttpRequest();
+    HTTPRequest.open('GET', 'https://trefle.io/api/plants/103505?token=Ymgra1d5M1dCaUlmMWgyME9qNVhTdz09&fbclid=IwAR3FY03yEVzS77Ca1Q9TIbMdMlJhXtpOjhcqcD-MJHAYJXCNcdA3UrJ2p9Q');
+    HTTPRequest.onload = function() {
+      var plantsArray = JSON.parse(HTTPRequest.responseText);
+      scope.printPlants(plantsArray);
+    };
+    HTTPRequest.send();
+}
+
+printPlants(plantsArray) {
+  var outputPlants = document.getElementById('plants_list');
+
+  outputPlants.innerHTML = '';
+
+  if (Array.isArray(plantsArray['plant'])) {
+    plantsArray['plants'].forEach((plant) => {
+      var strCategory = '';
+      var outputHTML = /*'<a href="recipe/' + plant['idPlant'] + '">*//* + '<StackLayout class="plant">' + '<Card:CardView class="cardStyle">' + '<StackLayout class="cardContent">' +
+                          '<Image src="' + plant['images.url'] + '">' +
+                          '<Label text="' + plant['common_name']  +
+                            '"></Label>' + '</Card:CardView> </StackLayout>' +
+                            
+      outputPlants.innerHTML = outputHTML;
+});
+*/
 }
