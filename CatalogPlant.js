f37e0497406a59be1d2ec940879e97774a84bdf9
ewabak <32634853+ewabak@users.noreply.github.com>
2019-05-23 17:14:16 +0200

    próba łączenia api w catalogplant.js

--- CatalogPlant.js
+++ CatalogPlant.js
@@ -16,28 +16,54 @@ import { withNavigation } from 'react-navigation';
 
 class CatalogPlant extends React.Component {
 
+  componentDidMount(){
+    const { navigation } = this.props;
+  const linkPlant = navigation.getParam('linkPlant');
+    fetch("https://trefle.io/api/plants/103505?token=Ymgra1d5M1dCaUlmMWgyME9qNVhTdz09&fbclid=IwAR3FY03yEVzS77Ca1Q9TIbMdMlJhXtpOjhcqcD-MJHAYJXCNcdA3UrJ2p9Q")
+    .then(response => response.json())
+   .then((responseJson)=> {
+     this.setState({
+      loading: false,
+      dataSource: responseJson
+     })
+   })
+   .catch(error=>console.log(error)) //to catch the errors if any
+   }
+
+   renderItem=(data)=>
+
+<TouchableOpacity style={styles.list}>
+              <Text style={styles.lightText}>{data.item.scientific_name}</Text> 
+     </TouchableOpacity>
+     
 
 render(){
 
-  const { navigation } = this.props;
-  const linkPlant = navigation.getParam('linkPlant');
+  
 
-     return(
-      <ScrollView style={styles.containerxd}>
-      
-     <View style={styles.white}>
-            <Button title="Add a new plant" type="solid" buttonStyle = {{backgroundColor:'#009C73'}} />
-          
-            <Text>{JSON.stringify(linkPlant)}</Text>
 
+  return(
+   <ScrollView style={styles.containerxd}>
+   <TouchableOpacity style={styles.textStyle}>
+      <Image
+         source={require('./images/burger.png')}
+         style={styles.ImageIconStyle} />
+  </TouchableOpacity>
+  <View style={styles.white}>
+  <View style={styles.space}/>
+  
+   
+  <View> 
+      <FlatList
+         data= {this.state.dataSource}
+         renderItem= {item=> this.renderItem(item)}
+         keyExtractor= {item=>item.id.toString()} />
       </View>
-
-    </ScrollView>
-     );}
-
-
-}
-
+   </View>
+ </ScrollView>
+  );
+  }
+    }
 export default CatalogPlant;
 
 
