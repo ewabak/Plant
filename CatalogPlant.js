77d884b7d210079289447c320093b082f6b35185
karolskoczyk <32647007+karolskoczyk@users.noreply.github.com>
2019-05-26 10:32:26 +0200

    wyswietlanie danych

--- CatalogPlant.js
+++ CatalogPlant.js
@@ -52,8 +52,18 @@ class CatalogPlant extends React.Component {
 render(){
   // console.log(this.state.data.dataSource);
   const { data } = this.state;
- 
-   
+  // const name = data && data.order && data.order.name || '';
+  const obrazek = data && data.main_species && data.main_species.images && data.main_species.images[0] && data.main_species.images[0].url || '';
+  const scname = data && data.scientific_name || '';
+  const coname = data && data.common_name || '';
+  const moisture = data && data.main_species && data.main_species.growth && data.main_species.growth.moisture_use || '';
+  const drought = data && data.main_species && data.main_species.growth && data.main_species.growth.drought_tolerance || '';
+  const fcolor = data && data.main_species && data.main_species.flower && data.main_species.flower.color || '';
+  const lifespan = data && data.main_species && data.main_species.specifications && data.main_species.specifications.lifespan || '';
+  const bloom = data && data.main_species && data.main_species.seed && data.main_species.seed.bloom_period || '';
+  const gperiod = data && data.main_species && data.main_species.specifications && data.main_species.specifications.growth_period || '';
+  const mheight = data && data.main_species && data.main_species.specifications && data.main_species.specifications.mature_height && data.main_species.specifications.mature_height.cm || '';
+  const toxicity = data && data.main_species && data.main_species.specifications && data.main_species.specifications.toxicity || '';
 
      return(
       <ScrollView style={styles.containerxd}>
@@ -67,48 +77,40 @@ render(){
      <View style={styles.white}>
 
             <View style={{flex:1, alignItems:'center', justifyContent:'center'}}> 
+
+      <Text>{obrazek}</Text>
+
+      <Image source = {obrazek} style = {{width: 160, height: 120}}/>
       
       <View style={styles.tabHeader}><Text style={styles.textHeader}>Scientific name</Text></View>
-      <View style={styles.tabContent}><Text style={styles.textContent}>{this.state.data.scientific_name}</Text></View>
+      <View style={styles.tabContent}><Text style={styles.textContent}>{scname}</Text></View>
       
       <View style={styles.tabHeader}><Text style={styles.textHeader}>Common name</Text></View>
-      <View style={styles.tabContent}><Text style={styles.textContent}>{this.state.data.common_name}</Text></View>
+      <View style={styles.tabContent}><Text style={styles.textContent}>{coname}</Text></View>
       
       <View style={styles.tabHeader}><Text style={styles.textHeader}>Moisture use</Text></View>
-      <View style={styles.tabContent}><Text style={styles.textContent}>{this.state.data.common_name}</Text></View>
+      <View style={styles.tabContent}><Text style={styles.textContent}>{moisture}</Text></View>
 
       <View style={styles.tabHeader}><Text style={styles.textHeader}>Drought tolerance</Text></View>
-      <View style={styles.tabContent}><Text style={styles.textContent}>{this.state.data.common_name}</Text></View>
+      <View style={styles.tabContent}><Text style={styles.textContent}>{drought}</Text></View>
 
       <View style={styles.tabHeader}><Text style={styles.textHeader}>Flower color</Text></View>
-      <View style={styles.tabContent}><Text style={styles.textContent}>{this.state.data.common_name}</Text></View>
+      <View style={styles.tabContent}><Text style={styles.textContent}>{fcolor}</Text></View>
       
       <View style={styles.tabHeader}><Text style={styles.textHeader}>Life span</Text></View>
-      <View style={styles.tabContent}><Text style={styles.textContent}>{this.state.data.common_name}</Text></View>
+      <View style={styles.tabContent}><Text style={styles.textContent}>{lifespan}</Text></View>
 
       <View style={styles.tabHeader}><Text style={styles.textHeader}>Bloom period</Text></View>
-      <View style={styles.tabContent}><Text style={styles.textContent}>{this.state.data.common_name}</Text></View>
+      <View style={styles.tabContent}><Text style={styles.textContent}>{bloom}</Text></View>
 
       <View style={styles.tabHeader}><Text style={styles.textHeader}>Growth period</Text></View>
-      <View style={styles.tabContent}><Text style={styles.textContent}>{this.state.data.common_name}</Text></View>
+      <View style={styles.tabContent}><Text style={styles.textContent}>{gperiod}</Text></View>
 
       <View style={styles.tabHeader}><Text style={styles.textHeader}>Mature height</Text></View>
-      <View style={styles.tabContent}><Text style={styles.textContent}>{this.state.data.common_name}</Text></View>
+      <View style={styles.tabContent}><Text style={styles.textContent}>{mheight} cm</Text></View>
 
       <View style={styles.tabHeader}><Text style={styles.textHeader}>Toxicity</Text></View>
-      <View style={styles.tabContent}><Text style={styles.textContent}>{this.state.data.common_name}</Text></View>
-
-
-
-
-
-      <Text>{this.state.data.shade_tolerance}</Text>
-      <Text>{this.state.data.moisture_use}</Text>
-      <Text>{this.images()}</Text>
-      
-
-
-      <Image source = {this.images()} style = {{width: 160, height: 120}}/>
+      <View style={styles.tabContent}><Text style={styles.textContent}>{toxicity}</Text></View>
 
       {/* <Image style={styles.image} source={stringify(this.images())} /> */}
 
