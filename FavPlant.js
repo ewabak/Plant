3876462658e910b17c72f6b8250006a3a94cbb52
Malakambla <malakambla@gmail.com>
2019-06-24 23:10:48 +0200

    photos

--- FavPlant.js
+++ FavPlant.js
@@ -7,7 +7,9 @@ import {
     ScrollView,
     ListView,
     FlatList,
-    StatusBar
+    StatusBar,
+    StyleSheet,
+    ActivityIndicator
 } from 'react-native';
 import styles from "./styles";
 import * as firebase from 'firebase';
@@ -69,6 +71,85 @@ class FavPlant extends React.Component {
       }
     })
      }
+     _maybeRenderUploadingOverlay = () => {
+      if (this.state.uploading) {
+        return (
+          <View
+            style={[
+              StyleSheet.absoluteFill,
+              {
+                backgroundColor: 'rgba(0,0,0,0.4)',
+                alignItems: 'center',
+                justifyContent: 'center',
+              },
+            ]}>
+            <ActivityIndicator color="#fff" animating size="large" />
+          </View>
+        );
+      }
+    };
+  
+    _maybeRenderImage = () => {
+      let { image } = this.state;
+      if (!image) {
+        return;
+      }
+  
+      return (
+        <View
+          style={{
+            marginTop: 30,
+            width: 250,
+            borderRadius: 3,
+            elevation: 2,
+          }}>
+          <View
+            style={{
+              borderTopRightRadius: 3,
+              borderTopLeftRadius: 3,
+              shadowColor: 'rgba(0,0,0,1)',
+              shadowOpacity: 0.2,
+              shadowOffset: { width: 4, height: 4 },
+              shadowRadius: 5,
+              overflow: 'hidden',
+            }}>
+            <Image source={{ uri: image }} style={{ width: 250, height: 250 }} />
+          </View>
+  
+          <Text
+            onPress={this._copyToClipboard}
+            onLongPress={this._share}
+            style={{ paddingVertical: 10, paddingHorizontal: 10 }}>
+            {image}
+          </Text>
+        </View>
+      );
+    };
+
+    _pickImage = async () => {
+      let pickerResult = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [4, 3],
+      });
+  
+      this._handleImagePicked(pickerResult);
+    };
+  
+    _handleImagePicked = async pickerResult => {
+      try {
+        this.setState({ uploading: true });
+  
+        if (!pickerResult.cancelled) {
+          uploadUrl = await uploadImageAsync(pickerResult.uri);
+          this.setState({ image: uploadUrl });
+        }
+      } catch (e) {
+        console.log(e);
+        alert('Upload failed, sorry :(');
+      } finally {
+        this.setState({ uploading: false });
+      }
+    };
 
 render(){
   let { image } = this.state;
@@ -106,6 +187,11 @@ render(){
               {/* {image &&
                 <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />} */}
               <View style={styles.space}/>
+
+              {this._maybeRenderImage()}
+              {this._maybeRenderUploadingOverlay()}
+
+        <StatusBar barStyle="default" />
 
             
            <Button title="Add plant to watering schedule" type="solid" 
@@ -166,5 +252,34 @@ render(){
   }
   export default FavPlant;
 
+
+async function uploadImageAsync(uri) {
+  // Why are we using XMLHttpRequest? See:
+  // https://github.com/expo/expo/issues/2402#issuecomment-443726662
+  const blob = await new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+      resolve(xhr.response);
+    };
+    xhr.onerror = function(e) {
+      console.log(e);
+      reject(new TypeError('Network request failed'));
+    };
+    xhr.responseType = 'blob';
+    xhr.open('GET', uri, true);
+    xhr.send(null);
+  });
+
+  const ref = firebase
+    .storage()
+    .ref()
+    .child(uuid.v4());
+  const snapshot = await ref.put(blob);
+
+  // We're done with the blob, close and release it
+  blob.close();
+
+  return await snapshot.ref.getDownloadURL();
+}
 
 
